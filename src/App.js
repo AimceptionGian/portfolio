a85864a9e0ffd9eccfc51744baedb064cd7c6d91
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-09-13 14:57:44 +0200

    styling and nav buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
-import milkyway from './images/milkyway.jpg';
+import white from './images/white.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div style={{ display: 'flex', justifyContent: 'flex-end', height: '100vh' }}>
-          <h1>Gian Oechslin</h1>
-          <img src={milkyway} style={{ width: '75%', objectFit: 'cover' }} />
+          <div style={{ display: 'flex', flexDirection: 'column', width: '40%', position: 'absolute', top: '50%', left: '25%', transform: 'translate(-50%, -50%)' }}>
+            <h1 className='First-name'>Gian</h1>
+            <h1 className='Name'>Oechslin</h1>
+          </div>
+          <div style={{position: 'absolute', width: '100%', height: '100%' }}>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
+              <ul>
+                <li>
+                  <div className='Nav-button'>Referenzen</div>
+                </li>
+                <li>
+                  <div className='Nav-button'>Interessen</div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img src={white} style={{ width: '75%', objectFit: 'cover' }} />
         </div>
       </header>
     </div>
