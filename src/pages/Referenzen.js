0df556445068851fdb6e1a4f5d35cde400c1c90a
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-11-08 15:09:59 +0100

    design von der referenzen seite optimiert

--- src/pages/Referenzen.js
+++ src/pages/Referenzen.js
@@ -17,14 +17,20 @@ export default function Referenzen() {
             <div style={{ display: 'flex', flexDirection: 'column', width: '100vh', zIndex: '2', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
                 <h1>Referenzen</h1>
             </div>
-            <div style={{ display: 'flex', flexDirection: 'column', width: '50vh', height: '100vh', zIndex: '1', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-                <img src={reference1} style={{ transform: 'translate(-50%, 39%)' }}/>
-                <img src={reference3} style={{ transform: 'translate(50%, -57%)' }} />
-                <div>
+            <div style={{ zIndex: '0', position: 'absolute', display: 'flex', flexDirection: 'column', justifyContent: 'center', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+                <div style={{ zIndex: '0', display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                    <img src={reference1} className='Referenzen-images' />
+                    <img src={reference3} className='Referenzen-images' />
+                </div>
+                <div style={{ zIndex: '0', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                    <img src={reference2} className='Referenzen-images' style={{ marginLeft: 'auto' }} />
+                    <div style={{ width: '25%' }}>
+                        <p style={{ width: '150%', marginLeft: '0%' }}>Hier sehen Sie eine Menge</p>
+                        <p style={{ width: '150%', marginLeft: '5%' }}>an spannenden Projekten, welche</p>
+                        <p style={{ width: '150%', marginLeft: '10%' }}>ich schon gemacht habe.</p>
+                    </div>
                     
                 </div>
-                <img src={reference2} style={{ transform: 'translate(0%, -57%)' }} />
-                <p>Hier sehen Sie ein paar Einblicke in meine Arbeit.</p>
             </div>
             <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
                 <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
